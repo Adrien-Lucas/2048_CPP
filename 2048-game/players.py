6eb6c0420d708722c7ec00cfeaaab9829352e15a
Adrien Lucas <mrbrenan@live.fr>
2019-03-25 16:08:58 +0100

    random_tile done !

--- 2048-game/players.py
+++ 2048-game/players.py
@@ -13,6 +13,19 @@ def random_direction(board):
 def random_tile(board):
     """returns a random 'tile move' on the board
         where tile 4 has only probably 1/9"""
+    zeros = []
+    for x in range(len(board)):
+        for y in range(len(board[x])):
+            if x == 0 and y == 0:
+                zeros.append([x,y])
+    
+    i = random.randint(0, len(zeros))
+    a = random.random()
+    if(a <= 1/9):
+        return(zeros[i][0], zeros[i][1], 2)
+    else:
+        return(zeros[i][0], zeros[i][1], 1)
+            
     raise NotImplementedError()
 
 
