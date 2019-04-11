041d7fbdab7328ff87b63f99d0cec7dab1a1a41c
Basile Lewandowski <lewandob@ensipc693.ensimag.fr>
2019-04-11 11:29:06 +0200

    Now with competitive playerz (not working)

--- 2048-game/players.py
+++ 2048-game/players.py
@@ -32,7 +32,7 @@ def random_tile(board):
 
 SIZE = 4
 EMPTYBOARD = [[0] * SIZE for i in range(SIZE)]
-print(random_tile(EMPTYBOARD))
+# print(random_tile(EMPTYBOARD))
 
 
 def first_direction(board):
@@ -95,7 +95,7 @@ def key(board):
     return res
 
 def coop_direction(board):
-    global MemoTile
+    global MemoDir
     MemoDir = {}
     if(rules.is_full(board)):
         return None
@@ -167,19 +167,84 @@ def coop_score_tile(board, depth):
                     zeros.append([x,y])
         m = 0
         ret = []
-        if(len(zeros) > 0):
-            for i in range(len(zeros)):
-                b = [board[i].copy() for i in range(SIZE)]
-                b[zeros[i][0]][zeros[i][1]] = 2
-                s = coop_score_dir(b, depth-1)
-                if s > m:
-                    m = s
-                    ret = b
+        for i in range(len(zeros)):
+            b = [board[i].copy() for i in range(SIZE)]
+            b[zeros[i][0]][zeros[i][1]] = 2
+            s = coop_score_dir(b, depth-1)
+            if s > m:
+                m = s
+                ret = b
         MemoTile[idx] = basic_coop_score(ret)
         return(basic_coop_score(ret))
     else :
         MemoTile[idx] = basic_coop_score(board)
         return basic_coop_score(board)
 
+
+####### Recursive comp players
+
+def comp_dir(board):
+    if (board != EMPTYBOARD) and (rules.game_over(board)):
+        ValueError()
+        return None
+    m = 0
+    a = 0
+    for i in rules.DIRECTIONS: 
+        if rules.move_dir(i,board) != board:
+            s = comp_score_dir(rules.move_dir(i,board), 4)
+            if s > m:
+                m = s
+                a = i        
+    return a
     
 
+def comp_tile(board):
+    zeros = []
+    for x in range(len(board)):
+        for y in range(len(board[x])):
+            if board[x][y] == 0:
+                zeros.append([x, y])
+    res = 0
+    a = 0
+    if(len(zeros) > 0):
+        for i in range(len(zeros)):
+            b = [board[i].copy() for i in range(SIZE)]
+            tst = [comp_score_tile(rules.move_tile(zeros[i] + [n], b), 4) for n in (1, 2)]
+            if min(tst) < res:
+                res = min(tst)
+                val = tst.index(min(tst)) + 1  # on joue 2 si les 2 tiles renvoient le même résultat
+                a = i
+        return (zeros[a][0], zeros[a][1], val)
+    else:
+        return None
+    
+
+def comp_score_dir(board, depth):
+    if depth > 0:
+        m = 0
+        for i in rules.DIRECTIONS: 
+            if rules.move_dir(i,board) != board:
+                s = comp_score_dir(rules.move_dir(i,board), depth - 1)
+                if s > m:
+                    m = s      
+        return m
+    else :
+        return basic_coop_score(board)   
+    
+    
+def comp_score_tile(board, depth):
+    if depth > 0 :
+        zeros = []
+        for x in range(SIZE):
+            for y in range(SIZE):
+                if board[x][y] == 0:
+                    zeros.append([x, y])
+        res = 0
+        for i in range(len(zeros)):
+            b = [board[i].copy() for i in range(SIZE)]
+            tst = [comp_score_tile(rules.move_tile(zeros[i] + [n], b), 4) for n in (1, 2)]
+            if min(tst) < res:
+                res = min(tst)
+        return res
+    else :
+        return basic_coop_score(board)
