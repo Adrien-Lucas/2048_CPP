211906dbeb6b7bc228e39e46c9534f3a408b186e
Basile <basile.lewan@gmail.com>
2019-04-14 23:15:50 +0200

    Competitive now working
    waw

--- 2048-game/players.py
+++ 2048-game/players.py
@@ -7,6 +7,7 @@ import config
 MemoDir = {}
 MemoTile = {}
 
+
 def random_direction(board):
     l = [d for d in rules.DIRECTIONS if rules.move_dir_possible(d, board)]
     if l:
@@ -31,6 +32,7 @@ def random_tile(board):
     else:
         return None
 
+
 SIZE = 4
 EMPTYBOARD = [[0] * SIZE for i in range(SIZE)]
 # print(random_tile(EMPTYBOARD))
@@ -46,10 +48,11 @@ def first_tile(board):
     for i in range(rules.LAST, -1, -1):
         for j in range(rules.LAST, -1, -1):
             if board[i][j] == 0:
-                return (i, j, 2)
-                
+                return i, j, 2
+
+
 def basic_coop_direction(board):
-    if(rules.is_full(board)):
+    if rules.is_full(board):
         return None
     m = 0
     a = 0
@@ -62,23 +65,24 @@ def basic_coop_direction(board):
     if m == 0:
         a = 3
     return a
-    
-def basic_coop_score(board):#Favoriser le serpentin
+
+
+def basic_coop_score(board):  # TODO : Favoriser le serpentin
     quality = 0
-    #m = 0
-    #m_tile = []
-    #for x in range(len(board)):
-    #    for y in range(len(board[x])):
-    #        if board[x][y] > m:
-    #            m = board[x][y]
+    # m = 0
+    # m_tile = []
+    # for x in range(len(board)):
+    #     for y in range(len(board[x])):
+    #         if board[x][y] > m:
+    #             m = board[x][y]
     #            m_tile = [x, y]
-    sorts = sorted(sorted(board[0])+sorted(board[1])+sorted(board[2])+ sorted(board[3]))
-    snaked = [sorts[0:4] , list(reversed(sorts[4:8])) , sorts[8:12] , list(reversed(sorts[12:16]))]
-    if board == snaked:
-        quality = 50
-        print("WOW")
+    # sorts = sorted(sorted(board[0])+sorted(board[1])+sorted(board[2])+ sorted(board[3]))
+    # snaked = [sorts[0:4] , list(reversed(sorts[4:8])) , sorts[8:12] , list(reversed(sorts[12:16]))]
+    # if board == snaked:
+    #     quality = 50
+    #     # print("WOW")
     
-    return (quality, rules.level(board))
+    return quality, rules.level(board)
 
 
 def basic_coop_tile(board):
@@ -89,7 +93,7 @@ def basic_coop_tile(board):
                 zeros.append([x,y])
     m = 0
     a = 0
-    if(len(zeros) > 0):
+    if len(zeros) > 0:
         for i in range(len(zeros)):
             b = [board[i].copy() for i in range(SIZE)]
             b[zeros[i][0]][zeros[i][1]] = 2
@@ -97,34 +101,37 @@ def basic_coop_tile(board):
             if s > m:
                 m = s
                 a = i
-        return (zeros[a][0], zeros[a][1], 2)
+        return zeros[a][0], zeros[a][1], 2
     else:
         return None
 
-#Recursive coop players
+# ---------- Recursive coop players ----------
+
 
 def key(board):
     """returns the value for the key of the board in the memo dict"""
-    res = ''.join(str(e) for i in range(rules.SIZE)for e in board[i])
-    res = hex(int(res)) # on passe en hexadécimal c'est moins foireux
+    res = ''.join(format(e, '02X') for i in range(rules.SIZE)for e in board[i])  # On utilise hex pour éviter ls doubles
+    res = hex(int(res, 16))  # on passe en hexadécimal pour réduire l'usage mémoire
     return res
+
 
 def coop_direction(board):
     global MemoDir
     MemoDir = {}
-    if(rules.is_full(board)):
+    if rules.is_full(board):
         return None
     m = ()
     a = 0
     for i in range(3): 
-        if rules.move_dir(i,board) != board:
-            s = coop_score_dir(rules.move_dir(i,board), config.DEPTH)
+        if rules.move_dir(i, board) != board:
+            s = coop_score_dir(rules.move_dir(i, board), config.DEPTH)
             if s > m:
                 m = s
                 a = i
     if m == ():
         a = 3
     return a
+
 
 def coop_score_dir(board, depth):
     idx = key(board)
@@ -134,8 +141,8 @@ def coop_score_dir(board, depth):
         m = ()
         a = 0
         for i in rules.DIRECTIONS: 
-            if rules.move_dir(i,board) != board:
-                s = coop_score_tile(rules.move_dir(i,board), depth-1)
+            if rules.move_dir(i, board) != board:
+                s = coop_score_tile(rules.move_dir(i, board), depth-1)
                 if s > m:
                     m = s
                     a = i
@@ -143,11 +150,12 @@ def coop_score_dir(board, depth):
             a = 3
         res = basic_coop_score(rules.move_dir(a, board))
         MemoDir[idx] = res
-        return(res)
-    else :
+        return res
+    else:
         MemoDir[idx] = basic_coop_score(board)
         return basic_coop_score(board)   
-    
+
+
 def coop_tile(board):
     global MemoTile
     MemoTile = {}
@@ -155,21 +163,20 @@ def coop_tile(board):
     for x in range(len(board)):
         for y in range(len(board[x])):
             if board[x][y] == 0:
-                zeros.append([x,y])
+                zeros.append([x, y])
     m = ()
     a = 0
-    if(len(zeros) > 0):
-        for i in range(len(zeros)):
-            b = [board[i].copy() for i in range(SIZE)]
-            b[zeros[i][0]][zeros[i][1]] = 2
-            s = coop_score_tile(b, config.DEPTH)
-            if s > m:
-                m = s
-                a = i
-        return (zeros[a][0], zeros[a][1], 2)
-    else:
-        return None
-    
+    assert len(zeros) > 0
+    for i in range(len(zeros)):
+        b = [board[i].copy() for i in range(SIZE)]
+        b[zeros[i][0]][zeros[i][1]] = 2
+        s = coop_score_tile(b, config.DEPTH)
+        if s > m:
+            m = s
+            a = i
+    return zeros[a][0], zeros[a][1], 2
+
+
 def coop_score_tile(board, depth):
     idx = key(board)
     if idx in MemoTile:
@@ -179,7 +186,7 @@ def coop_score_tile(board, depth):
         for x in range(len(board)):
             for y in range(len(board[x])):
                 if board[x][y] == 0:
-                    zeros.append([x,y])
+                    zeros.append([x, y])
         m = ()
         ret = []
         for i in range(len(zeros)):
@@ -190,23 +197,23 @@ def coop_score_tile(board, depth):
                 m = s
                 ret = b
         MemoTile[idx] = basic_coop_score(ret)
-        return(basic_coop_score(ret))
-    else :
+        return basic_coop_score(ret)
+    else:
         MemoTile[idx] = basic_coop_score(board)
         return basic_coop_score(board)
 
 
-####### Recursive comp players
+# ---------- Recursive comp players ----------
 
 def comp_dir(board):
-    if (board != EMPTYBOARD) and (rules.game_over(board)):
-        ValueError()
-        return None
-    m = ()
+    #  Inutile ici : le game over est géré par play2048
+    # if (board != EMPTYBOARD) and (rules.game_over(board)):
+    #     return None
+    m = 0
     a = 0
     for i in rules.DIRECTIONS: 
-        if rules.move_dir(i,board) != board:
-            s = comp_score_dir(rules.move_dir(i,board), config.DEPTH)
+        if rules.move_dir(i, board) != board:
+            s = comp_score_dir(rules.move_dir(i, board), config.DEPTH)
             if s > m:
                 m = s
                 a = i        
@@ -214,52 +221,58 @@ def comp_dir(board):
     
 
 def comp_tile(board):
+    """Tile player trying to return the position of the tile to add leading to the minimum score
+    ACHTUNG : for some reason, only works with depth below 2"""
+
     zeros = []
     for x in range(len(board)):
         for y in range(len(board[x])):
             if board[x][y] == 0:
                 zeros.append([x, y])
-    res = 0
+    m = 1e16
     a = 0
-    if(len(zeros) > 0):
-        for i in range(len(zeros)):
-            b = [board[i].copy() for i in range(SIZE)]
-            tst = [comp_score_tile(rules.move_tile(zeros[i] + [n], b), config.DEPTH) for n in (1, 2)]
-            if min(tst) < res:
-                res = min(tst)
-                val = tst.index(min(tst)) + 1  # on joue 2 si les 2 tiles renvoient le même résultat
+
+    for i in range(len(zeros)):
+        b1, b2 = ([board[i].copy() for i in range(SIZE)] for k in'##')
+        b1[zeros[i][0]][zeros[i][1]], b2[zeros[i][0]][zeros[i][1]] = 1, 2
+        tst = [comp_score_tile(b,  config.DEPTH) for b in (b1, b2)]
+        if min(tst) < m:
+                m = min(tst)
                 a = i
-        return (zeros[a][0], zeros[a][1], val)
-    else:
-        return None
-    
+                val = tst.index(min(tst)) + 1
+    return zeros[a][0], zeros[a][1], val
+
 
 def comp_score_dir(board, depth):
     if depth > 0:
-        m = ()
+        m = 0
         for i in rules.DIRECTIONS: 
-            if rules.move_dir(i,board) != board:
-                s = comp_score_dir(rules.move_dir(i,board), depth - 1)
+            if rules.move_dir(i, board) != board:
+                s = comp_score_dir(rules.move_dir(i, board), depth - 1)
                 if s > m:
                     m = s      
         return m
-    else :
-        return basic_coop_score(board)   
+    else:
+        return rules.level(board)
     
     
 def comp_score_tile(board, depth):
-    if depth > 0 :
+    """Returns the minimum score available for a specified depth & board"""
+    if depth > 0:
         zeros = []
-        for x in range(SIZE):
-            for y in range(SIZE):
+        for x in range(len(board)):
+            for y in range(len(board[x])):
                 if board[x][y] == 0:
                     zeros.append([x, y])
-        res = 0
+        m = 1e16
+        Outb = []
         for i in range(len(zeros)):
-            b = [board[i].copy() for i in range(SIZE)]
-            tst = [comp_score_tile(rules.move_tile(zeros[i] + [n], b), config.DEPTH) for n in (1, 2)]
-            if min(tst) < res:
-                res = min(tst)
-        return res
-    else :
-        return basic_coop_score(board)
+            b1, b2 = ([board[i].copy() for i in range(SIZE)] for k in '##')
+            (rules.move_tile(zeros[i] + [n], b) for (n, b) in ((1, b1), (2, b2)))
+            tst = [comp_score_tile(b,  depth - 1) for b in (b1, b2)]
+            if min(tst) < m:
+                m = min(tst)
+                Outb = (b1, b2)[tst.index(min(tst))]
+        return rules.level(Outb)
+    else:
+        return rules.level(board)
--- 2048-game/validation_debug.py
+++ 2048-game/validation_debug.py
@@ -4,17 +4,31 @@ import rules
 import validation_tools as vt
 import players
 import mean_score
+import random
 
 # INSERT YOUR TESTS HERE
 
-# r = mean_score.game_direction_first(vt.play_None, players.first_tile, rules.STEP0)
-# print(r)
 
-r = players.first_tile(rules.EMPTYBOARD)
-q = mean_score.game_tile_first(players.first_direction, players.first_tile, rules.EMPTYBOARD)
-print(q)
-print(r)
-# print(rules.move_dir(0, [[2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [3, 2, 2, 2]]))
+def Key_tst(scale):
+    """Test if there is 2 different boards with the same key on a specified random sample"""
+    dico = {}
+    for k in range(scale):
+        tstboard = [[random.randint(0, 15) for i in range(rules.SIZE)] for i in range(rules.SIZE)]
+        idx = players.key(tstboard)
+        if idx in dico:
+            try:
+                assert dico[idx] == tstboard
+            except AssertionError:
+                print('ERREUR :\n{0}\nET\n{1}\nONT LA MÊME CLÉE !!!!'.format(dico[idx], tstboard))
+                return
+        dico[idx] = tstboard
+
+# --------- TESTS ---------
+vt.start_test('Key on random board')
+Key_tst(10)
+Key_tst(500)
+Key_tst(int(1e5))
 
 
+vt.valid_current_stage("Clées de Mémoïsation")
 
